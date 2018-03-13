9c7f0077f0bfd93ce1fc7259336d4359b772eba9
Yuske Fukuyama <yuskefuku@gmail.com>
2018-03-13 20:29:03 +0900

    unique

---
+++ unique.swift
@@ -0,0 +1,29 @@
+func isUnique(_ input: String) -> Bool {
+  var checkLetter = [Character]()
+  for letter in input.characters {
+    if checkLetter.contains(letter) {
+      return false
+    }
+    checkLetter.append(letter)
+  }
+
+	return true
+}
+
+func isUnique2(_ input: String) -> Bool {
+  return Set(input.characters).count == input.characters.count
+}
+
+// call function
+print(isUnique("AaBbCc"))         // true
+print(isUnique("Hello, world!"))  // false
+print(isUnique2("AaBbCc"))         // true
+print(isUnique2("Hello, world!"))  // false
+// Swift 4
+func isUnique3(_ input: String) -> Bool {
+    return Set(input).count == input.count
+}
+
+
+print(isUnique3("AaBbCc"))         // true
+print(isUnique3("Hello, world!"))  // false

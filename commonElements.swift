5ef016fb1e2a103ac8e113d1a27a132a009f3537
Yuske Fukuyama <yuskefuku@gmail.com>
2018-05-30 16:30:04 +0900

    eupho

--- commonElements.swift
+++ commonElements.swift
@@ -22,5 +22,21 @@ func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> [Int]
         return matchedItems.unique()
 }
 
+// better?
+//func anyCommonElements<T: Collection, U: Collection>(_ lhs: T, _ rhs: U) -> [T.Element] where T.Element: Hashable, T.Element == U.Element {
+//    var matchedItems = [T.Element]()
+//
+//    for lhsElement in lhs {
+//        print("lhs element: \(lhsElement)")
+//        for rhsElement in rhs {
+//            if lhsElement == rhsElement {
+//                matchedItems.append(lhsElement)
+//            }
+//        }
+//    }
+//
+//    return Array(Set(matchedItems))
+//}
+
 anyCommonElements([1, 2, 3, 4, 4], [4, 5, 6, 7])
 anyCommonElements([1, 2, 3], [4, 5, 6])

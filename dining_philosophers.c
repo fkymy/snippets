2f68bdef22732946f6229ac195584dcb06708706
fkymy <yuskefuku@gmail.com>
2021-03-10 20:48:58 +0900

    change implementation to cv

--- dining_philosophers.c
+++ dining_philosophers.c
@@ -5,40 +5,34 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-#define SECTONSEC 1000000000 /* sec to nanosec */
-# define USECTONSEC 1000 /* microsec to nanosec */
-# define MSECTOUSEC 1000 /* milisec to microsec */
-
 typedef struct	s_params
 {
 	int id;
 	pthread_mutex_t *left;
 	pthread_mutex_t *right;
 	int *permits;
-	pthread_mutex_t *permits_lock;
+	pthread_cond_t *cv;
+	pthread_mutex_t *m;
 }				t_params;
 
 static const int kNumPhilosophers = 5;
 static const int kNumForks = kNumPhilosophers;
 static const int kNumMeals = 3;
 
-void wait_for_permission(int *permits, pthread_mutex_t *permits_lock) {
-	while (1)
-	{
-		pthread_mutex_lock(permits_lock);
-		if (*permits > 0)
-			break ;
-		pthread_mutex_unlock(permits_lock);
-		usleep(100);
-	}
+static void wait_for_permission(int *permits, pthread_cond_t *cv, pthread_mutex_t *m) {
+	pthread_mutex_lock(m);
+	while (permits == 0)
+		pthread_cond_wait(cv, m);
 	(*permits)--;
-	pthread_mutex_unlock(permits_lock);
+	pthread_mutex_unlock(m);
 }
 
-void grant_permission(int *permits, pthread_mutex_t *permits_lock) {
-	pthread_mutex_lock(permits_lock);
+static void grant_permission(int *permits, pthread_cond_t *cv, pthread_mutex_t *m) {
+	pthread_mutex_lock(m);
 	(*permits)++;
-	pthread_mutex_unlock(permits_lock);
+	if (*permits == 1)
+		pthread_cond_broadcast(cv);
+	pthread_mutex_unlock(m);
 }
 
 static void think(int id) {
@@ -46,13 +40,13 @@ static void think(int id) {
 	sleep(1);
 }
 
-static void eat(int id, pthread_mutex_t *left, pthread_mutex_t *right, int *permits, pthread_mutex_t *permits_lock) {
-	wait_for_permission(permits, permits_lock);
+static void eat(int id, pthread_mutex_t *left, pthread_mutex_t *right, int *permits, pthread_cond_t *cv, pthread_mutex_t *m) {
+	wait_for_permission(permits, cv, m);
 	pthread_mutex_lock(left);
 	pthread_mutex_lock(right);
 	printf("%d is eating\n", id);
 	sleep(1);
-	grant_permission(permits, permits_lock);
+	grant_permission(permits, cv, m);
 	pthread_mutex_unlock(left);
 	pthread_mutex_unlock(right);
 }
@@ -63,19 +57,18 @@ static void *philosopher(void *args) {
 	p = (t_params *)args;
 	for (int i = 0; i < kNumMeals; i++) {
 		think(p->id);
-		eat(p->id, p->left, p->right, p->permits, p->permits_lock);
+		eat(p->id, p->left, p->right, p->permits, p->cv, p->m);
 	}
 	return NULL;
 }
 
 int main(int argc, char *argv[]) {
-	pthread_mutex_t forks[kNumPhilosophers];
+	pthread_mutex_t forks[kNumPhilosophers], m;
+	pthread_cond_t cv;
 	pthread_t philosophers[kNumPhilosophers];
 	t_params params[kNumPhilosophers];
 
 	int permits = kNumPhilosophers - 1;
-	pthread_mutex_t permits_lock;
-	pthread_mutex_init(&permits_lock, NULL);
 
 	for (int i = 0; i < kNumPhilosophers; i++) {
 		pthread_mutex_init(&forks[i], NULL);
@@ -83,7 +76,8 @@ int main(int argc, char *argv[]) {
 		params[i].left = &forks[i];
 		params[i].right = &forks[(i + 1) % kNumPhilosophers];
 		params[i].permits = &permits;
-		params[i].permits_lock = &permits_lock;
+		params[i].cv = &cv;
+		params[i].m = &m;
 		pthread_create(&philosophers[i], NULL, philosopher, (void *)(params + i));
 	}
 	for (int j = 0; j < kNumPhilosophers; j++) {

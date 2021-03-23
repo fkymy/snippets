a2b35c257813df67ceb835d2a35a9c9eaa136e51
fkymy <yuskefuku@gmail.com>
2021-03-23 22:50:56 +0900

    do pipeline

--- microshell.c
+++ microshell.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <errno.h>
 #include <string.h>
+
+#define OP_PIPE 2
+#define OP_SEQUENTIAL 3
+
+int exit_status;
+
+void report(const char *fmt, va_list params)
+{
+	vfprintf(stderr, fmt, params);
+}
+
+void die(const char *fmt, ...)
+{
+	va_list params;
+
+	va_start(params, fmt);
+	report(fmt, params);
+	va_end(params);
+	exit(exit_status);
+}
 
 typedef struct command command;
 struct command
@@ -37,8 +60,6 @@ static void command_append_arg(command *c, char *word) {
 
 static void command_free(command *c)
 {
-    /* for (int i = 0; i < c->argc; i++) */
-    /*     free(c->argv[i]); */
     free(c->argv);
     free(c);
 }
@@ -57,12 +78,83 @@ static void command_clear(command **c)
 	}
 }
 
+static pid_t start_command(char *arg[], int ispipe, int haspipe, int lastpipe[2])
+{
+	pid_t pid;
+	int newpipe[2];
+
+	// if pipe, pipe
+	// fork
+	// execute command
+	// cleanup pipe
+	// setup next pipe
+
+	if (ispipe)
+		pipe(newpipe);
+	pid = fork();
+	if (pid < 0)
+		die("failed to fork: %s\n", strerror(errno));
+	else if (pid == 0)
+	{
+		// child
+
+	}
+	// parent
+
+	return pid;
+}
+
+static command *do_pipeline(command *c)
+{
+	int ispipe;
+	int haspipe;
+	int lastpipe[2];
+
+	ispipe = 0;
+	haspipe = 0;
+	lastpipe[0] = -1;
+	lastpipe[1] = -1;
+	while (c)
+	{
+		ispipe = (c->op == OP_PIPE);
+		c->pid = start_command(c->argv, ispipe, haspipe, lastpipe);
+		haspipe = ispipe;
+		if (haspipe && c->next)
+			c = c->next;
+		else if (haspipe && !c->next)
+			die("multiline pipeline is not implemented\n");
+		else
+			break ;
+	}
+	return NULL;
+}
+
 void run_list(command *c)
 {
+	pid_t pid;
+	int status;
+
 	while (c)
 	{
 		for (int i = 0; i < c->argc; i++)
 			printf("argv: %s\n", c->argv[i]);
+
+		// Check for builtin
+
+		// Do pipeline
+		c = do_pipeline(c);
+
+		// Wait pipeline
+		pid = waitpid(c->pid, &status, 0);
+		assert(pid == c->pid);
+		if (WIFEXITED(status))
+			exit_status = WEXITSTATUS(status);
+		else
+			die("child %d exited abnormally", pid);
+
+		// Reap zombie processes
+		while (wait(NULL) > 0);
+
 		c = c->next;
 	}
 }
@@ -86,6 +178,6 @@ int main(int argc, char *argv[])
 
 	// Command clear
 	command_clear(&c);
-	return 0;
+	return exit_status;
 }
 

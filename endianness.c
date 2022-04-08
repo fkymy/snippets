e60eb61f8e1b48840d9d73232853bf73885737f8
fkymy <yuskefuku@gmail.com>
2022-04-09 03:03:58 +0900

    determine endianness

---
+++ endianness.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	{
+		int x = 1;
+		if(*(char *)&x == 1)
+			printf("little-endian\n");
+		else
+		printf("big-endian\n");
+	}
+
+	{
+		union {
+			int i;
+			char c[sizeof(int)];
+		} x;
+		x.i = 1;
+		if(x.c[0] == 1)
+			printf("little-endian\n");
+		else
+			printf("big-endian\n");
+	}
+}
